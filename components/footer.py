330a12d097a8cda699e08dc0f4f10959f015804c
Tanmoy Das <tanmoydas.haldia2021@gmail.com>
2026-07-21 11:19:41 +0530

    Update

--- components/footer.py
+++ components/footer.py
@@ -5,11 +5,15 @@ def show_footer():
     st.divider()
 
     st.markdown(
-        """
-        <div style="text-align:center; padding:10px;">
-            🌦️ <b>Climate Visibility Intelligence Dashboard</b><br>
-            Built  using Streamlit, Scikit-learn, XGBoost & SHAP
-        </div>
-        """,
-        unsafe_allow_html=True,
-    )
+    """
+    <div style="
+        text-align:center;
+        padding:10px;
+        color:inherit;
+    ">
+        🌦️ <b>Climate Visibility Intelligence Dashboard</b><br>
+        Built using Streamlit, Scikit-learn, XGBoost & SHAP
+    </div>
+    """,
+    unsafe_allow_html=True,
+)
